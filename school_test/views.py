91b375ef597a13d5c536e64da9b4783f0af3fc2f
Rafael-Benicio <jerebenicio@gmail.com>
2024-01-10 18:08:08 -0300

    feat : Adição de multipos status code pra evidenciar a resposta do serviço

--- school_test/views.py
+++ school_test/views.py
@@ -17,7 +17,7 @@ def user_login_validate(request)->HttpResponse:
           if not (ph.verify(student_entity.password, request.POST['password'])):
                raise "Wrong Password"
      except:
-          return render(request,"school_test/login.html",{"error_message": "Seu Id ou Senha estão errados","current_id":request.POST["id_student"],"current_password":request.POST["password"]})
+          return render(request,"school_test/login.html",{"error_message": "Seu Id ou Senha estão errados","current_id":request.POST["id_student"],"current_password":request.POST["password"]},status=401)
      else:
           response= HttpResponseRedirect(reverse("school_test:home",))
           response.set_cookie('id_student_cookie', request.POST['id_student'])
@@ -28,7 +28,7 @@ def home(request)->HttpResponse:
           id_student= int(request.COOKIES['id_student_cookie'])
      except KeyError as err:
           print(f' Erro, Cookie : {err} not finded')
-          return HttpResponseRedirect(reverse("school_test:login",))
+          return render(request,"school_test/login.html",{"error_message": "Ocorreu algum erro com seu acesso","current_id":'',"current_password":''},status=401)
      else:
           context = {"student_tests": build_list_of_tests(id_student)}
           return render(request,"school_test/home.html",context)
@@ -45,9 +45,12 @@ def student_test(request,id_test:int)->HttpResponse:
      
 
 def result_calculate(request)->HttpResponse:
-     grade=calculate_test_grade(remove_identification_data(request.POST))
-     regist_student_test_grade(request.POST,grade)
-     return HttpResponseRedirect(reverse("school_test:home",))
+     try: 
+          grade=calculate_test_grade(remove_identification_data(request.POST))
+          regist_student_test_grade(request.POST,grade)
+          return HttpResponseRedirect(reverse("school_test:home",))
+     except:
+          return render(request,"school_test/login.html",{"error_message": "Ocorreu algum erro com seu acesso","current_id":'',"current_password":''},status=403)
 
 def user_logout(request)->HttpResponse:
      response= HttpResponseRedirect(reverse("school_test:login",))
