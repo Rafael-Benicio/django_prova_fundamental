d72fd99d3e50f1e5af9582a791d23d89f828a983
Rafael-Benicio <jerobenicio@gmail.com>
2024-01-14 01:23:06 -0300

    test: concerta erros nos formularios de logins em testes

--- school_test/tests.py
+++ school_test/tests.py
@@ -1,22 +1,23 @@
 from django.contrib.auth import get_user
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from .models import Question, ReadyTest, Student, TestQuestions, TestToDo
+from .models import Question, ReadyTest, TestQuestions, TestToDo
 
 
 class AcessePagesTestCase(TestCase):
     def setUp(self):
         self.client = Client()
 
-        self.student = Student.objects.create(
+        self.student = User.objects.create(
             first_name="Rafael",
             last_name="Benicio",
             password="abc123456",
             username="Rafaelzinho",
         )
 
-        self.student_2 = Student.objects.create(
+        self.student_2 = User.objects.create(
             first_name="Lucas",
             last_name="Germania",
             password="123abcdef",
@@ -52,11 +53,13 @@ class AcessePagesTestCase(TestCase):
         self.test_question.save()
         self.test_to_do.save()
 
+    # login
     def test_acesse_login_page(self):
         response = self.client.get(reverse("school_test:login"))
 
         self.assertEqual(response.status_code, 200)
 
+    # admin
     def test_redirect_to_admin_page(self):
         response = self.client.get("/admin")
 
@@ -71,6 +74,7 @@ class AcessePagesTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("school_test:home"))
 
+    # validade
     def test_redirect_student_with_wrong_password_to_login_page(self):
         login_form = {"username": "Rafaelzinho", "password": "abc1ddd23459"}
 
@@ -87,6 +91,7 @@ class AcessePagesTestCase(TestCase):
         self.assertEqual(response.url, reverse("school_test:login"))
         self.assertEqual(response.status_code, 302)
 
+    # home
     def test_redirect_acesse_in_home_page_without_autentication(self):
         response = self.client.get(reverse("school_test:home"))
 
@@ -96,11 +101,14 @@ class AcessePagesTestCase(TestCase):
     def test_acesse_home_page_with_autentication(self):
         login_form = {"username": "Rafaelzinho", "password": "abc123456"}
 
-        response = self.client.post(reverse("school_test:validate"), login_form)
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
 
-        self.assertEqual(response.url, reverse("school_test:home"))
-        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse("school_test:home"))
 
+        self.assertEqual(response.status_code, 200)
+
+    # test
     def test_acesse_test_page_with_autentication(self):
         login_form = {"username": "Rafaelzinho", "password": "abc123456"}
 
@@ -131,19 +139,40 @@ class AcessePagesTestCase(TestCase):
             f"/school_test/login/?next=/school_test/home/prova/{self.test.id}",
         )
 
+    # result
     def test_redirect_not_expected_acesse_in_result_path(self):
         response = self.client.get(reverse("school_test:result"))
 
         self.assertEqual(response.url, "/school_test/login/?next=/school_test/result/")
         self.assertEqual(response.status_code, 302)
 
-    def test_redirect_expected_acesse_in_result_path(self):
+    def test_redirect_expected_acesse_in_result_path_with_right_info(self):
         test_form = {
             "id_student": str(self.student.id),
             "id_test": str(self.test.id),
             str(self.question.id): "2",
         }
-        login_form = {"username": "Luquinhas123", "password": "123abcdef"}
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
+
+        response = self.client.post(reverse("school_test:result"), test_form)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("school_test:home"))
+        self.assertTrue(
+            TestToDo.objects.get(
+                id_student=self.student.id, id_test=self.test.id
+            ).was_done
+        )
+
+    def test_redirect_expected_acesse_in_result_path_but_with_wrong_form_1(self):
+        test_form = {
+            "id_student": str(self.student.id),
+            "id_test": str(self.test.id),
+            "wd": "2",
+        }
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
 
         self.client.force_login(self.student)
         self.client.post(reverse("school_test:validate"), login_form)
@@ -152,8 +181,86 @@ class AcessePagesTestCase(TestCase):
 
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse("school_test:home"))
+        self.assertFalse(
+            TestToDo.objects.get(
+                id_student=self.student.id, id_test=self.test.id
+            ).was_done
+        )
 
-    def test_redirect_acesse_in_logout(self):
-        response = self.client.get(reverse("school_test:logout"))
+    def test_redirect_expected_acesse_in_result_path_but_with_wrong_form_2(self):
+        test_form = {
+            "id_student": str(self.student.id),
+            "id_test": str(self.test.id),
+        }
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
+
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
+
+        response = self.client.post(reverse("school_test:result"), test_form)
+
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, reverse("school_test:login"))
+        self.assertEqual(response.url, reverse("school_test:home"))
+        self.assertFalse(
+            TestToDo.objects.get(
+                id_student=self.student.id, id_test=self.test.id
+            ).was_done
+        )
+
+    def test_redirect_expected_acesse_in_result_path_but_with_wrong_form_3(self):
+        test_form = {
+            "id_student": str(self.student.id),
+            str(self.question.id): "2",
+        }
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
+
+        response = self.client.post(reverse("school_test:result"), test_form)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("school_test:home"))
+        self.assertFalse(
+            TestToDo.objects.get(
+                id_student=self.student.id, id_test=self.test.id
+            ).was_done
+        )
+
+    def test_redirect_expected_acesse_in_result_path_but_with_wrong_form_4(self):
+        test_form = {
+            "id_test": str(self.test.id),
+            str(self.question.id): "2",
+        }
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
+
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
+
+        response = self.client.post(reverse("school_test:result"), test_form)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("school_test:home"))
+        self.assertFalse(
+            TestToDo.objects.get(
+                id_student=self.student.id, id_test=self.test.id
+            ).was_done
+        )
+
+    # logout
+    def test_redirect_acesse_in_logout_path_and_logout(self):
+        login_form = {"username": "Rafaelzinho", "password": "abc123456"}
+
+        self.client.force_login(self.student)
+        self.client.post(reverse("school_test:validate"), login_form)
+
+        response_1 = self.client.get(reverse("school_test:home"))
+        response_2 = self.client.get(reverse("school_test:logout"))
+        response_3 = self.client.get(reverse("school_test:home"))
+
+        self.assertEqual(response_1.status_code, 200)
+
+        self.assertEqual(response_2.status_code, 302)
+        self.assertEqual(response_2.url, reverse("school_test:login"))
+
+        self.assertEqual(response_3.status_code, 302)
+        self.assertEqual(response_3.url, "/school_test/login/?next=/school_test/home/")
