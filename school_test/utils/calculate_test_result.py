956e56acc70a3022e28a36bf06da26c6403a7ef0
Rafael-Benicio <jerobenicio@gmail.com>
2024-01-14 01:31:46 -0300

    feat : adiciona função que garante que aluno possa refazer a prova em caso de erro na subimição

--- school_test/utils/calculate_test_result.py
+++ school_test/utils/calculate_test_result.py
@@ -1,4 +1,4 @@
-from ..models import Student, TestToDo, ReadyTest, Question, TestQuestions
+from ..models import Question, ReadyTest, TestQuestions, TestToDo
 
 
 def calculate_test_grade(id_question_and_choice: dict) -> float:
@@ -19,6 +19,15 @@ def regist_student_test_grade(test_form: dict, grade: float) -> None:
     )
     if not (student_test.was_done):
         student_test.grade = grade
+        student_test.was_done = True
+        student_test.save()
+
+
+def enable_student_to_make_the_test(test_form: dict) -> None:
+    student_test = TestToDo.objects.get(
+        id_test=int(test_form["id_test"]), id_student=int(test_form["id_student"])
+    )
+    if not (student_test.was_done):
         student_test.was_done = True
         student_test.save()
 
