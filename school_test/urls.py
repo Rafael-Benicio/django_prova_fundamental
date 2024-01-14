68c61e2349301a0ef3779b7fcb1d0de5e7e613bd
Rafael-Benicio <jerobenicio@gmail.com>
2024-01-14 01:24:50 -0300

    refactor: troca nome de login view para um mais apropriado

--- school_test/urls.py
+++ school_test/urls.py
@@ -5,7 +5,7 @@ import school_test.views
 app_name = "school_test"
 
 urlpatterns = [
-    path("", school_test.views.login, name="login"),
+    path("login/", school_test.views.user_login, name="login"),
     path("logout/", school_test.views.user_logout, name="logout"),
     path("validate_user/", school_test.views.user_login_validate, name="validate"),
     path("result/", school_test.views.result_calculate, name="result"),
--- testForFundamental/urls.py
+++ testForFundamental/urls.py
@@ -1,10 +1,10 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import include, path
+
 import school_test.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("school_test/", include("school_test.urls")),
-    path("", school_test.views.login),
+    path("", school_test.views.user_login),
 ]
